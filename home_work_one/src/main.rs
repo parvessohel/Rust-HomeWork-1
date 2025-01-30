1ecbcd02037ba57893fb8dd84d7a1d6e1f58a21e
engineeratxx <engineer@xenonxplore.com>
2025-01-30 20:11:09 +0600

    The let keyword with explanation comment

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let x;
-    x = 5;
-
-    println!("The value of X is {}", x);
+    let x;          // Variable x is declared with the keyword "let" without initializing it.
+    x = 5;              // Variable x is assigned with value 5. It works because it is declared before but wasn't assigned any value.
+    println!("The value of X is {}", x);                // Variable x is printed with the help of println! macro and {}.        
 
 }
