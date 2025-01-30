f357c3ae33cce8ed2b3d229686368477bcd39bea
engineeratxx <engineer@xenonxplore.com>
2025-01-30 19:54:51 +0600

    HomeWork One Initiated

---
+++ home_work_one/target/debug/deps/home_work_one-8d7a0f97b3d9a2a2.d
@@ -0,0 +1,5 @@
+C:\Users\parve\Desktop\Rust\Projects\Rust-HomeWork-1\home_work_one\target\debug\deps\libhome_work_one-8d7a0f97b3d9a2a2.rmeta: src/main.rs
+
+C:\Users\parve\Desktop\Rust\Projects\Rust-HomeWork-1\home_work_one\target\debug\deps\home_work_one-8d7a0f97b3d9a2a2.d: src/main.rs
+
+src/main.rs:
---
+++ home_work_one/target/debug/deps/home_work_one-c369e190c8a961de.d
@@ -0,0 +1,5 @@
+C:\Users\parve\Desktop\Rust\Projects\Rust-HomeWork-1\home_work_one\target\debug\deps\libhome_work_one-c369e190c8a961de.rmeta: src/main.rs
+
+C:\Users\parve\Desktop\Rust\Projects\Rust-HomeWork-1\home_work_one\target\debug\deps\home_work_one-c369e190c8a961de.d: src/main.rs
+
+src/main.rs:
