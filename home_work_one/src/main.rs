78312c8f4d1f0026d887b9ac79881bcdfa42ed37
engineeratxx <engineer@xenonxplore.com>
2025-01-30 20:23:55 +0600

    Type Annotation example

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let x;          // Variable x is declared with the keyword "let" without initializing it.
-    x = 5;              // Variable x is assigned with value 5. It works because it is declared before but wasn't assigned any value.
-    println!("The value of X is {}", x);                // Variable x is printed with the help of println! macro and {}.        
+    let x: i32; // Variable x is declared with Type annotation that is i32. It is an unsigned integer.
+    x = 5; // Variable x is assigned with value 5. It works because it is declared before but wasn't assigned any value.
+    println!("The value of X is {}", x); // Variable x is printed with the help of println! macro and {}.
 
+    // let x: i32 = 5; It can also be initialized and assiged at the same time with Type annotation attached to it.
 }
+
