048c5bc6de76753a09577854d7cb2aa5a66baf27
engineeratxx <engineer@xenonxplore.com>
2025-01-31 23:47:10 +0600

    Destructering a Tuple

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,4 +1,16 @@
+fn sh1kari(stupid_char: String, stupid_number: i32) -> (String, i32) {
+    println!("{}{}", stupid_char, stupid_number);
+    (stupid_char, stupid_number) // Returning a Tuple.
+}
+
 fn main() {
-  let pair = ('x', 42);     // This is a Tuple. You can store multiple values of different types in a single variable with Tuples
-  println!("{}", pair.0);       // Tuples can be accessed with a dot "." with their corresponding index number as you do in an Array.
+    let (stupid_char, stupid_number) = ('x', 42); // Tuples are Destructured and assigned to its corresponding variables.
+    println!("{} {}", stupid_char, stupid_number);
+
+    let (stupid_char_one, stupid_number_1) = sh1kari(stupid_char.to_string(), stupid_number); // You can also call a function that returns a Tuple and destructure the result.
+    println!("{} {}", stupid_char_one, stupid_number_1);
+
+    let (_, stupid_number) = (",", 42); // You can also throw away a specific value of a Tuple using "_".
+
+    println!("{}", stupid_number);
 }
