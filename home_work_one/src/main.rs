8a8734d4f65fec991bf8f341e5ed9096980ee8b2
engineeratxx <engineer@xenonxplore.com>
2025-01-31 22:51:35 +0600

    How Tuples Work

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,12 +1,4 @@
-fn get_sh1kari(x: i32) {
-    println!("The value of X is an Idiot {}", x);
-}
-
 fn main() {
-    let x = 13;
-    get_sh1kari(x);
-    let x = x + 3;
-    get_sh1kari(x); // -> Here this x is the second one. As soon as a new variable is declared with the same name the earlier one is dropped and cannot be accessed. This is called Shadowing in Rust
-                    // It is specailly useful if we want to change the type of the variable and we want to avoid declaring new variables with a different name to improve readability.
-                    // Shadowing essentially lets you create a new variable with the same name.
+  let pair = ('x', 42);     // This is a Tuple. You can store multiple values of different types in a single variable with Tuples
+  println!("{}", pair.0);       // Tuples can be accessed with a dot "." with their corresponding index number as you do in an Array.
 }
