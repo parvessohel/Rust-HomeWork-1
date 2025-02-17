3bd7c025cde74c255f304a2f11de666df8ca67dd
engineeratxx <engineer@xenonxplore.com>
2025-02-18 00:40:16 +0600

    Explanation of function with a return type

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,8 +1,14 @@
+fn value_of_x(z: i32) ->i32{                  // This is a function that takes a parameter z of i32 and also returns a value of i32. The arrow indicates that teh function has a return type.
+    println!("Value of x is {}", z);
+    z // This function returns z and we dont neeed to to use the keyword "return" or semicolon.
+}
+
 fn main() {
     let x = 4;
     let y = 2;
     let z = x + y; // Semicolon marks the end of a statement.
-    println!("Value of X is {}", z);
+    let omega = value_of_x(z);
+    println!("Value of Omega is {}", omega);
 
     let x = vec![4, 2, 0]
     .iter()
