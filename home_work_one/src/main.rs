d5afe5534e8e9e624e47ee92733ffa12cb04e889
engineeratxx <engineer@xenonxplore.com>
2025-02-18 04:28:49 +0600

    Explained crate, modules and method with use keywordx

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,12 +1,12 @@
-fn idiot(x: String) -> String {
-    let size = x.len().to_string(); // We can also call a function with a dot like here.
-    size
-}
-
+use std::cmp::max; // `use` is used to bring `max` into scope from the `std` crate's `cmp` module.
+                   // use std::cmp::min; // This imports min
+                   // use std::cmp::{max, min}; // We can also import like this.
+                   // use std::{cmp::max, cmp::min}; // This also imports `max` and `min`, same as above.
+                   // use std::cmp::*; This imports all items from the `cmp` module, including `min` and `max`.
 fn main() {
-    let x = (10, 11);
-    println!("The first value of x is {}", x.0); // This is how we access a field value in Rust using dot.
-    let sopa: String = "quickerthancoffee".to_string();
-    let size = idiot(sopa);
-    println!("Length of Sopa is {}", size)
+    let smaller = std::cmp::min(4, 2); // Here std is a crate, cmp is a module, and min is a method. We can use those namespaces with double colon.
+    println!("The Smaller number is {}", smaller);
+
+    let bigger = max(4, 2);  // We can use max directly because we imported it with "use" keyword above.
+    println!("The Bigger numer is {}", bigger);
 }
