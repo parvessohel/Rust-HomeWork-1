8150d9c6f2317483644405126699426e6d6c7e67
engineeratxx <engineer@xenonxplore.com>
2025-02-17 23:56:13 +0600

    End of statement explaanation with example

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,16 +1,12 @@
-fn sh1kari(stupid_char: String, stupid_number: i32) -> (String, i32) {
-    println!("{}{}", stupid_char, stupid_number);
-    (stupid_char, stupid_number) // Returning a Tuple.
-}
-
 fn main() {
-    let (stupid_char, stupid_number) = ('x', 42); // Tuples are Destructured and assigned to its corresponding variables.
-    println!("{} {}", stupid_char, stupid_number);
+    let x = 4;
+    let y = 2;
+    let z = x + y; // Semicolon marks the end of a statement.
+    println!("Value of X is {}", z);
 
-    let (stupid_char_one, stupid_number_1) = sh1kari(stupid_char.to_string(), stupid_number); // You can also call a function that returns a Tuple and destructure the result.
-    println!("{} {}", stupid_char_one, stupid_number_1);
-
-    let (_, stupid_number) = (",", 42); // You can also throw away a specific value of a Tuple using "_".
-
-    println!("{}", stupid_number);
+    let x = vec![4, 2, 0]
+    .iter()
+    .map(|x| x + 3)
+    .fold(0, |x, y| x + y); // This expression has multiple lines, but the semicolon at the end marks the end of the full statement.
+    println!("The value of x is {}", x);
 }
