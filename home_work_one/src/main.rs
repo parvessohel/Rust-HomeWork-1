b3831be2dc34558c761a7fe5c4d0dfb456c0370e
engineeratxx <engineer@xenonxplore.com>
2025-02-18 01:42:05 +0600

    Explaing blocks works as an expression

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,15 +1,15 @@
-fn value_of_x(x: i32) -> i32 {
-    println!("Value of x is {}", x);
-    x
-}
-
 fn main() {
     let x = 4;
+    println!("Value of X is {}", x);
+    let x = { 42 }; // Same as the first one.
+    println!("Value of X is {}", x);
 
-    let omega = value_of_x(x);
-    println!("Value of X and Omega is {} and {}", x, omega);
-    {
-        let x = vec![4, 2, 0].iter().map(|x| x + 3).fold(0, |x, y| x + y);
-        println!("The value of x is {}", x); // This x and the previous x are different. This x is inside of a curly braces. That mean it is inside a block of its own.
-    }
+    let x = {
+        // Curly braces in Rust define an expression block,
+        let y = 4;
+        let z = 2;
+        y + z // allowing multiple operations like performing complex calculations, function calls, loops, and if-else blocks etc..
+    };
+
+    println!("Value of X is {}", x);
 }
