e2724e7788b1fcf644f3cde9af3a4f2d3c822820
engineeratxx <engineer@xenonxplore.com>
2025-02-18 06:31:41 +0600

    Destructureing explained with examles

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     #[derive(Debug)]
     struct Vec2 {
-        // Structs are declared here.
         x: i32,
         y: i32,
         z: i32,
     }
-    let x1 = Vec2 { x: 4, y: 2, z: 0 }; // Here Structs are initialized.
-    println!("Value of x is {:?}", x1);
-    let x2 = Vec2 { x: 42, ..x1 }; // We can initialize rest of the fields from another struct in this way. This is called Stuct Update Syntax
-    println!("Value of x2 is {:?}", x2);
-    let x3 = Vec2 { ..x2 }; // All the fields are initialized her in this way.
-    println!("Value of x2 is {:?}", x3);
+
+    let v = Vec2 { x: 4, y: 2, z: 0 };
+    let Vec2 { x, y, z } = v; // Structs as well as Tuples can be destructered like this.
+    println!("Value of x is {:?}", v);
+
+    let Vec2 { x, .. } = v; // Values can also be dropped while destructering like here y and z's values are dropped.
+    println!("The value of Vec2 is {:?}", x);
 }
