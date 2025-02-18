1c7dc6c41912981be603a41f032add0216687c82
engineeratxx <engineer@xenonxplore.com>
2025-02-18 06:10:00 +0600

    Struct update syntax explained with example

--- home_work_one/src/main.rs
+++ home_work_one/src/main.rs
@@ -4,7 +4,12 @@ fn main() {
         // Structs are declared here.
         x: i32,
         y: i32,
+        z: i32,
     }
-    let x1 = Vec2 { x: 4, y: 2 }; // Here Structs are initialized.
+    let x1 = Vec2 { x: 4, y: 2, z: 0 }; // Here Structs are initialized.
     println!("Value of x is {:?}", x1);
+    let x2 = Vec2 { x: 42, ..x1 }; // We can initialize rest of the fields from another struct in this way. This is called Stuct Update Syntax
+    println!("Value of x2 is {:?}", x2);
+    let x3 = Vec2 { ..x2 }; // All the fields are initialized her in this way.
+    println!("Value of x2 is {:?}", x3);
 }
